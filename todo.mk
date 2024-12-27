5152df3ae692a786bdf00c71d0abde47d8184434
Eldar Kersebaum <kersebaum.eldar@gmail.com>
2024-12-27 18:14:26 +0100

    Added Vision to the todo list

--- todo.mk
+++ todo.mk
@@ -10,3 +10,25 @@
 
 ## Bugs
 - Wandbug Fixen  []
+
+
+
+## Grundlage:
+Aiming mit Maus passieren (Teeworlds ähnlich)
+
+Waffen vorerst von Teeworlds übernehmen: Pistole, Shotgun, Granatwerfer, Laser
+
+## Vision
+Ramon:
+	Startwaffe kann einmal schießen, mobs spawnen coin, die mann einsammeln muss.
+	Wenn man stirbt, kommt man ins hauptmenü und kann sich von den gesammelten coins upgrades kaufen.
+	Man soll sich auch andere Waffen kaufen können.
+	Player Perks: Wall Slides, Double Jumps
+
+Eldar:
+	Mann soll verschiedene Waffen freischalten können.
+	Verschiedene Tiers von upgrades fände ich cool. Zum Beispiel das man Tier1 upgrades wie Leben, Schaden, Attack Speed etc günstig erwerben kann, und Tier2 Upgrades wie neue Waffen oder upgrades der Waffen (Mehr Projektile, mehr Bullet Bounces etc) welche um eingies teurer sind.
+
+Lars:
+	Waffe mit Bounce effekt!!!
+	Aktive Fähigkeiten
